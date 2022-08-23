a0d109e26c9638021952c9862b68ac38f8ca0dbb
Nyahyun <91472283+nyahyun@users.noreply.github.com>
2022-08-24 02:13:24 +0900

    const and variable

---
+++ 02_01_CircleArea/02_01_CircleArea.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const double PI{ 3.141592 };	//원주율 정의
+	double radius;	//원의 반경
+
+	cout << "원의 반경을 입력하시오 : ";
+	cin >> radius;		//원의 반경 입력
+	double area = radius * radius * PI;	//면적 계산
+	cout << "원의 면적 = " << area << endl;
+	return 0;
+}
