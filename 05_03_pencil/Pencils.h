8cab8691d0a8c047c540b9a9d476c7c142a72abb
Nyahyun <91472283+nyahyun@users.noreply.github.com>
2022-11-24 21:57:07 +0900

    operator_overloading

---
+++ 05_03_pencil/Pencils.h
@@ -0,0 +1,15 @@
+#ifndef PENCILS_H_INCLUDED
+#define PENCILS_H_INCLUDED
+class Pencils {
+	int dozens;		// ´Ù½º
+	int np;			// ³¹°³
+public:
+	Pencils() : dozens(0), np(0) { };
+	Pencils(int n)
+		{ dozens = n / 12; np = n % 12; }
+	Pencils(int d, int n) : dozens(d), np(n) { }
+	Pencils& operator++();
+	Pencils operator++(int);
+	void display() const;
+};
+#endif
